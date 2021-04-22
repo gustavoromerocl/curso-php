30dcc552b667e771cfd388b4342d26188a3c6ce4
romerodevelopments <gandresrp@gmail.com>
2021-04-22 14:05:29 -0400

    Configurando el entorno - Niveles de error

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+    ##Configurando el entorno
     echo 'hola ', 'mundo ';
     print 'Hola mundo con print';
+    
+    //Niveles de error
+    // Parsed error - Fatal error - Notice error
+    //Custom error
+    trigger_error("Error al escribir", E_USER_ERROR);
 ?>
