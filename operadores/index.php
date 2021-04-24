22e3ebc4397865ecc3b6b24fd57e71150a990564
romerodevelopments <gandresrp@gmail.com>
2021-04-24 13:17:08 -0400

    Operadores - Comparacion

---
+++ operadores/index.php
@@ -0,0 +1,19 @@
+<?php echo "OPERADORES \n"?>
+
+<?php
+##Comparacion
+$age='18';
+
+echo var_dump($age===18)
+/*
+== Igual
+=== Identico: compara valor y tipo de dato
+!= o <> Distinto
+!== No identico: Distinto valor o distinto tipo
+> Mayor que
+< Menor que  
+>= MAyor o igual que
+<= Menor o igual que
+<=> Nave espacial : -1 menor que, 0 iguales, 1 mayor que
+*/
+?>
