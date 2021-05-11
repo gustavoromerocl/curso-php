a5888aec0d2249eaf7ce24c4a44ab5575b6706be
romerodevelopments <gandresrp@gmail.com>
2021-05-11 14:33:37 -0400

    Funciones - Funciones

---
+++ funciones/index.php
@@ -0,0 +1,14 @@
+<?php echo "FUNCIONES \n";?>
+
+<?php
+    echo "FUNCIONES \n";
+
+    function subjects(){
+        echo "Matematicas, Biologia, Español, Ciencias, Geografia \n";
+    }
+
+    echo "Jose, tus materias son: ";
+    subjects();
+    echo "Karla, tus materias son: ";
+    subjects();
+?>
