2105f61de23512d937ad42432a3618019fa2842e
romerodevelopments <gandresrp@gmail.com>
2021-05-11 14:50:40 -0400

    Funciones - Argumentos de funciones

--- funciones/index.php
+++ funciones/index.php
@@ -12,3 +12,41 @@
     echo "Karla, tus materias son: ";
     subjects();
 ?>
+
+<?php
+    echo "ARGUMENTOS DE FUNCIONES \n";
+
+    function divideBy($divisor, $dividend=1){
+        $result=$divisor/$dividend;
+        echo "El resultado de la división es: $result \n";
+    }
+
+    divideBy(10,2);
+    divideBy(5);
+
+    ##Pasar valores por referencia usando &
+
+    function plus(&$param1, $param2){
+        $param1=$param1+$param2;
+        echo 'El resultado es '.$param1;
+    }
+
+    $arg1=20;
+    $arg2=5;
+
+    plus($arg1,$arg2);
+
+    echo "\nEl argumento 1 es $arg1 \n";
+
+
+    ##Funciones recursivas
+
+    function call($value){
+        if($value <= 10){
+            echo "$value \n";
+            call(++$value);
+        }
+    }
+
+    call(1);
+?>
