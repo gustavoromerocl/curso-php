c9bc5e9b3c232a8fe4d5fbd4c5fee4ec966fd9b3
romerodevelopments <gandresrp@gmail.com>
2021-05-12 23:37:12 -0400

    Programacion orientada a objetos - Clases y atributos

---
+++ programacion-orientada-a-objetos/index.php
@@ -0,0 +1,26 @@
+<?php echo "PROGRAMACION ORIENTADA A OBJETOS \n";?>
+
+<?php echo "Clases y atributos \n";?>
+<?php
+class Person{
+    $name;
+    $lastname;
+    $sex;
+    $nationality;
+    $age;
+
+    function run(){
+
+    }
+
+    function see(){
+        echo "Yo veo una camisa azul";
+        $this->name; 
+        $this->run();
+    }
+
+    function talk(){
+
+    }
+}
+?>
