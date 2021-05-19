cf1f17e1fe674bd79cc78f47e0a768c673b04f59
romerodevelopments <gandresrp@gmail.com>
2021-05-19 17:29:38 -0400

    Desarrollo web - Redireccionar

--- desarrollo-web/form.php
+++ desarrollo-web/form.php
@@ -8,6 +8,8 @@ $phone=$_REQUEST['phone'];
 $sex=$_REQUEST['sex'];
 
 echo "Nombre $name Apellido $lastname Direccion $adress Telefono $phone Sexo $sex";
+
+//Header es usado para redireccionar a otro archivo, una vez que la solicitud del formulario es llamada mediante post y validada la condicion, en esta linea es redireccionada nuevamente a index.php
 header("location:index.php?answer=Tus datos han sido recibidos&name=gracias {$name}"); 
 }
 ?>
