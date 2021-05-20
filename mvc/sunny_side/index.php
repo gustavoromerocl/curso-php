9edbf4a55cc9d76b6c54eea622e3c8b5dda81d09
romerodevelopments <gandresrp@gmail.com>
2021-05-20 17:59:11 -0400

    MVC - Que es mvc

---
+++ mvc/sunny_side/index.php
@@ -0,0 +1,24 @@
+<?php
+##https://www.php.net/manual/es/book.pdo.php
+##El el fichero php.ini se puede habilitar otros motores de base de datos, por defecto apache trae pdo mysql y sqlite
+
+//print_r(PDO::getAvailableDrivers()); //Consulta las base de datos activas
+require_once 'model/animal.php';
+
+$animal = new Animal();
+print_r($animal->getAll());
+
+$animal->id=3;
+$animal->name='Ozzy';
+$animal->specie='Perro';
+$animal->breed='kiltro';
+$animal->gender='Macho';
+$animal->age='12';
+$animal->color='Dorado';
+
+//$animal->create();
+
+//$animal->delete(2);
+
+$animal->update();
+?>
